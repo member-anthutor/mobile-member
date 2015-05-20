cc6ad6ae897c3ee0fe7c14f802d1a1b4dc558915
Wanghuamin <huamin.wang@travelzen.com>
2015-05-20 17:08:54 +0800

    remove setting

--- script/member.sh
+++ script/member.sh
@@ -1,4 +1,4 @@
 selfpath=$(cd "$(dirname "$0")"; pwd)
-proj_dir=$(cd "$(dirname "$selfpath/../tops-mobile-member/..")"; pwd)
+proj_dir=$(cd "$(dirname "$selfpath/../..")"; pwd)
 
 gradle -p $proj_dir --parallel --refresh-dependencies cE e
